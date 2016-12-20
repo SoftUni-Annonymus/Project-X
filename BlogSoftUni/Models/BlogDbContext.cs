aee7697cd9754181e2fa4bbf41ba33424a619705
PlamenMarinov94 <plamen_marinov94@abv.bg>
2016-12-20 18:48:53 +0200

    Design changes and bug fixes.

--- BlogSoftUni/Models/BlogDbContext.cs
+++ BlogSoftUni/Models/BlogDbContext.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using BlogSoftUni.Models.ImageUploadApp.Models;
 
 namespace BlogSoftUni.Models
 {
@@ -15,6 +16,7 @@ namespace BlogSoftUni.Models
         public virtual IDbSet<Article> Articles { get; set; }
         public virtual IDbSet<Category> Categories { get; set; }
         public  virtual IDbSet<Article.Tag> Tags { get; set; }
+        public virtual IDbSet<Image> Images { get; set; }
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
