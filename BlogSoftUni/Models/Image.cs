aa0048bb9686d56125a6b784317021689de82553
PlamenMarinov94 <plamen_marinov94@abv.bg>
2016-12-20 18:51:32 +0200

    hh

---
+++ BlogSoftUni/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogSoftUni.Models
+{
+    namespace ImageUploadApp.Models
+    {
+        using System;
+        using System.Collections.Generic;
+
+        public partial class Image
+        {
+            public int ID { get; set; }
+            public string ImagePath { get; set; }
+        }
+    }
+}
